3bb1e1457f97db1a707533a3df3dffc936da49a4
fasterthansky <fasterthansky@gmail.com>
2020-02-10 11:04:09 -0800

    completed bees sprint, built classes using pseudoclassical style

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,20 @@
 var ForagerBee = function() {
+  Bee.call(this);
+
+  this.age = 10;
+  this.job = 'find pollen';
+  this.color;
+  this.canFly = true;
+  this.food;
+  this.treasureChest = [];
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.eat = function() {
+};
+
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+};
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,22 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.color;
+  this.food;
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.eat = function (){
+
+};
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+};
